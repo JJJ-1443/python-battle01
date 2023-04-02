fbc7149f5cc5b34a0496a21f240c567c157f0907
JJJ-1443 <nbv1443@gmail.com>
2023-04-02 12:24:09 +0900

    공격기능을 추가하면 공격의 이름이 출력되도록 수정. input에 안적어도 됌. 함수에 mp가 소모량보다 적으면 continue 되도록 어떻게 할 수 있지?..

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Character:
         self.hp = hp
         self.max_hp = hp
         self.power = power
+        self.actions = {"1": {'verbose': '일반공격', 'action1': self.nomal_attack}}
 
     def nomal_attack(self, target):
         damage = random.randint(self.power - 2, self.power + 2)
@@ -27,6 +28,8 @@ class Player(Character):
         self.mp = mp
         self.max_mp = mp
         self.magic_power = magic_power
+        self.actions["2"] = {'verbose': '마법공격', 'action1': self.magic_attack}
+        self.actions["3"] = {'verbose': '스페셜공격', 'action1': self.spcial_attack}
 
     def magic_attack(self, target):
         damage = random.randint(self.magic_power - 10, self.magic_power + 10)
@@ -36,6 +39,9 @@ class Player(Character):
         print(f'{self.name}이 {target.name}에게 {damage}의 마법공격 데미지를 입혔습니다.')
         if target.hp == 0:
             print(f'{target.name}이 쓰러졌습니다.')
+
+    def spcial_attack(self, target):
+        print(f'{self.name}이 {target.name}에게 스페셜 어택 데미지를 입혔습니다.')
 
     def check_status(self):
         print(
@@ -64,20 +70,27 @@ while player.hp > 0 or monster.hp > 0:
     player.check_status()
     monster.check_status()
     print('=' * 50)
-    action = input('행동을 선택해주세요. 1.일반공격 / 2.마법공격(MP 20소모) / exit 게임종료')
+    for k, v in player.actions.items():
+        print(f"{k} : {v['verbose']}", end=", ")
+    action = input('행동을 선택해주세요.')
     print('=' * 50)
-    if action == "1":
-        player.nomal_attack(monster)
-    elif action == "2":
-        if player.mp < 20:
-            print('MP가 부족합니다!')
-            continue
-        player.magic_attack(monster)
-    elif action == 'exit':
-        sys.exit()
-    else:
-        print('제대로 입력해주세요!')
+    if action not in player.actions:
+        print('정확한 값을 입력해주세요')
         continue
+    player.actions[action]['action1'](monster)
+
+    # if action == "1":
+    #     player.nomal_attack(monster)
+    # elif action == "2":
+    #     if player.mp < 20:
+    #         print('MP가 부족합니다!')
+    #         continue
+    #     player.magic_attack(monster)
+    # elif action == 'exit':
+    #     sys.exit()
+    # else:
+    #     print('제대로 입력해주세요!')
+    #     continue
 
     if monster.hp == 0:
         print('승리')
